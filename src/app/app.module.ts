9474877a13fcdb90021e76eb43e2661c5d4faa4a
AigarJ <aigar134@gmail.com>
2022-04-09 14:09:47 +0300

    test

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import { NewsFeedComponent } from './components/news-feed/news-feed.component';
+import { NewsFormComponent } from './components/news-form/news-form.component';
+
+@NgModule({
+  declarations: [
+    WelcomeComponent,
+    NewsFeedComponent,
+    NewsFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [WelcomeComponent]
+})
+export class AppModule { }
---
+++ src/app/services/news-feed.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {map, Observable, tap} from "rxjs";
+import {Post} from "../models/post";
+import {BACKEND_CREATE_POST, BACKEND_NEWS_FEED} from "../constants/constant";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsFeedService {
+
+  constructor(private http: HttpClient) { }
+
+  // Observable objects are lazy
+  getRecentPosts(): Observable<Array<Post>> {
+   return  this.http.get<any>(BACKEND_NEWS_FEED)
+      .pipe(
+        tap(value => console.log("data before processing: " + JSON.stringify(value))),
+        map((value: Array<any>) =>
+        value.map(item =>
+        <Post>{
+         id: item.id,
+          header: item.header,
+          content: item.content,
+          author: item.author,
+          creationTimestamp: item.creation_timestamp,
+          updateTimestamp: item.update_timestamp
+         })),
+        tap(value => console.log("data after processing " + JSON.stringify(value)))
+      )
+  }
+
+  createNewPost(newPost: Post){
+    console.log(`Trying to send to backend new post: " [${newPost}]`)
+    this.http.post(BACKEND_CREATE_POST, newPost)
+  }
+}
